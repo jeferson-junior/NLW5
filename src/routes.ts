23a6f5333d621b7f0988f1793cd5f7e62df7789c
Jeferson Junior <jeferson@live.in>
2021-04-21 17:14:29 -0300

    Nodejs Aula 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+//import { getCustomRepository } from "typeorm";
+import { SettingsController } from "./controllers/SettingsController";
+//import { SettingsRepository } from "./repositories/SettingsRepository";
+//import { UsersRepository } from "./repositories/UsersRepository";
+import { UsersController } from "./controllers/UsersController";
+//import { MessagesRepository } from "./repositories/MessagesRepository";
+import { MessagesController } from "./controllers/MessagesController";
 
 const routes = Router();
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.get('/', (req, res) => {
     return res.json({
@@ -10,30 +18,16 @@ routes.get('/', (req, res) => {
     });
 })
 
+routes.get('/messages/:id', messagesController.showByUser);
+
 routes.post('/', (req, res) => {
     return res.json({
         Rota: "POST /",
     });
 })
 
-routes.post('/users', (req, res) => {
-    return res.json({
-        Rota: "POST /users",
-    });
-})
-
-routes.post('/settings', async (req, res) => {
-    const { chat, username } = req.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    });
-
-    await settingsRepository.save(settings);
-
-    return res.json(settings);
-});
+routes.post('/messages', messagesController.create);
+routes.post('/settings', settingsController.create);
+routes.post('/users', usersController.create);
 
 export { routes };
